b4a9569b5c471543e232491403e2c13201a03820
skorobogatovamail <skorobogatovamail@gmail.com>
2024-06-13 20:35:20 +0300

    release 2: add auth Slice

---
+++ client/src/redux/slices/auth/authSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice, isAction } from '@reduxjs/toolkit';
+import type { AuthState } from '../../../types/authTypes';
+import { loginThunk, refreshThunk, signupThunk } from './authThunks';
+
+const initialState: AuthState = {
+  accessToken: '',
+  user: {
+    status: 'pending',
+  },
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(signupThunk.fulfilled, (state, action) => {
+        const { accessToken, user } = action.payload;
+        state.accessToken = accessToken;
+        state.user = user;
+      })
+      .addCase(loginThunk.fulfilled, (state, action) => {
+        const { accessToken, user } = action.payload;
+        state.accessToken = accessToken;
+        state.user = user;
+      })
+      .addCase(refreshThunk.fulfilled, (state, action) => {
+        const { accessToken, user } = action.payload;
+        state.accessToken = accessToken;
+        state.user = user;
+      })
+      .addCase(refreshThunk.rejected, (state, action) => {
+        state.accessToken = '';
+        state.user = { status: 'guest' };
+      })
+      .addCase(loginThunk.fulfilled, (state, action) => {
+        state.accessToken = '';
+        state.user = { status: 'guest' };
+      });
+  },
+});
+
+const authReducer = authSlice.reducer;
+
+export default authReducer;
