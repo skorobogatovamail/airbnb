e3fb23f4cc8238918d5bc4ff49906388e9f344e9
skorobogatovamail <skorobogatovamail@gmail.com>
2024-06-17 14:49:35 +0300

    add entries slice

---
+++ client/src/redux/slices/entriesFirebase/entriesFirebaseSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { EntryStateType } from '../../../types/entriesTypes';
+import {
+  deleteEntryThunk,
+  getAllEntriesThunk,
+  getOneEntryThunk,
+  postEntryThunk,
+  updateEntryThunk,
+} from './entriesFirebaseThunks';
+
+const initialState: EntryStateType = {
+  entries: [],
+  currentEntry: null,
+  favorites: [],
+};
+
+export const entrySlice = createSlice({
+  name: 'entriesFirebase',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllEntriesThunk.fulfilled, (state, action) => {
+        state.entries = action.payload;
+      })
+      .addCase(getOneEntryThunk.fulfilled, (state, action) => {
+        state.currentEntry = action.payload;
+      })
+      .addCase(postEntryThunk.fulfilled, (state, action) => {
+        state.entries.push(action.payload);
+      })
+      .addCase(updateEntryThunk.fulfilled, (state, action) => {
+        const idx = state.entries.findIndex((el) => el.key === action.payload.key);
+        state.entries[idx] = action.payload;
+      })
+      .addCase(deleteEntryThunk.fulfilled, (state, action) => {
+        state.entries = state.entries.filter((el) => el.key !== action.payload);
+      });
+  },
+});
+
+const entryReducer = entrySlice.reducer;
+
+export default entryReducer;
